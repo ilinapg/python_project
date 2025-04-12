e7c6fac1cbe44336b642b82912b2b3f32718ee9f
ilinapg <polika_ilina@mail.ru>
2025-04-12 15:02:04 +0300

    updated paths in .py files

--- proj_dict/urls.py
+++ proj_dict/urls.py
@@ -1,4 +1,4 @@
-"""proj_maths URL Configuration
+"""proj_dict URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
